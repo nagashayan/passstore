8e1869f9c1a40e54f9d80e1b1789de156f3389de
Nagashayan <nagashayan1@gmail.com>
2017-04-04 18:33:26 -0400

    updation working fine
    CURD operation is done

--- angularjs/maincontroller.js
+++ angularjs/maincontroller.js
@@ -10,7 +10,8 @@
             sname: null,
             url: null,
             name: null,
-            password: null
+            password: null,
+            recordtype: 'new'
 
         };
 
@@ -33,6 +34,8 @@
             $scope.data.push(response.rows);
             console.log($scope.data[0]);
 
+            //reset form
+            resetForm();
            // $scope.data.push(JSON.stringify(response.rows));
           }, function(error) {
               console.log("ERROR -> " + error);
@@ -45,65 +48,115 @@
    });
 
 
-       // $scope.init();
+       //frame the variable and return
 
+       $scope.initvar = function(index) {
 
+           console.log("initing");
+
+           return "show_"+index+" =  true";
+
+       }
 
         //5. create submitkeepassForm() function. This will be called when user submits the form
         $scope.submitkeepassForm = function() {
 
             $scope.myTxt = "You clicked submit!";
-
+            console.log($scope);
             //saving the record
             if ($scope.record.name) {
                 console.log("keepass record saved" + $scope.record.name);
-                console.log("keepass record saved" + $scope.record.url);
-                var record = {
-                    sname: $scope.record.sname,
-                    url: $scope.record.url,
-                    name: $scope.record.name,
-                    password: $scope.record.password
-                };
+                console.log("keepass record saved" + $scope.record.recordtype);
 
-               // $scope.data.push(record);
+
 
                 //store the record locally
-                storeData(record);
+                if ($scope.record.recordtype == "new"){
+                 var record = {
+                     sname: $scope.record.sname,
+                     url: $scope.record.url,
+                     name: $scope.record.name,
+                     password: $scope.record.password
+                 };
+                  console.log("creating");
+                  storeData(record);
 
-                //reset form
-                $scope.record.sname = '';
-                $scope.record.url = '';
-                $scope.record.name = '';
-                $scope.record.password = '';
-                record = null;
+
+                 }
+                 else{
+                  //update
+                  console.log("updating");
+                  console.log($scope.record);
+                  updateData($scope.record);
+                 }
+
+
             }
 
         };
 
-        //frame the variable and return
 
-        $scope.initvar = function(index) {
-
-            console.log("initing");
-
-            return "show_"+index+" =  true";
-
-        }
 
 
         //store the data
         var storeData = function(record) {
-            console.log("storing data locally");
-
-
+            console.log("storing data locally"+record.id+record._id);
+             console.log(record);
             $pouchDB.save(record).then(function(response) {
               console.log("saving success");
-
+              console.log(response);
               //reload database
               $scope.init();
             }, function(error) {
                 console.log("ERROR -> " + error);
             });
+
+        }
+
+        //remove the data
+        $scope.updateItem = function(record) {
+
+            console.log("updating form"+record.doc._id+record.doc._rev);
+
+
+             $scope.record = {
+
+              sname: record.doc.sname,
+              url: record.doc.url,
+              name: record.doc.name,
+              password: record.doc.password,
+              recordtype: record.doc._id
+             };
+
+
+        }
+
+        var updateData = function(updatedrecord) {
+
+            console.log("updating data locally"+updatedrecord.recordtype);
+              console.log(updatedrecord);
+
+           $pouchDB.get(updatedrecord.recordtype).then(function(response) {
+              console.log("getting success");
+              console.log(response);
+
+              //udate record with new values
+
+              response.sname = updatedrecord.sname;
+              response.url = updatedrecord.url;
+              response.name = updatedrecord.name;
+              response.password = updatedrecord.password;
+
+              console.log("before store data");
+              console.log(response);
+
+              //save the updated record
+              storeData(response);
+
+            }, function(error) {
+                console.log("ERROR -> " + error);
+            });
+
 
         }
 
@@ -126,8 +179,14 @@
 
         }
 
-        //6. create resetForm() function. This will be called on Reset button click.
-        $scope.resetForm = function() {
-            $scope.keepass = angular.copy($scope.Originalkeepass);
+        //reset form to blank state
+        var resetForm = function() {
+
+            $scope.record.sname = '';
+            $scope.record.url = '';
+            $scope.record.name = '';
+            $scope.record.password = '';
+            $scope.record.recordtype = 'new';
+            record = null;
         };
     });
