e4c59f55b9142d8699b260d49b9d5a4383ceddf2
Nagashayan <nagashayan1@gmail.com>
2017-04-23 15:13:36 -0400

    added g+plus login
    its not working yet because google signing auth requires http protocol
    where as electron used ftp by default

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const BrowserWindow = electron.BrowserWindow
 
 const path = require('path')
 const url = require('url')
+//var proxy = require('proxy-middleware'); // proxy for npm
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
